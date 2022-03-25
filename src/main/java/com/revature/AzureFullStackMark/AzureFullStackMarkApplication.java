9284f8ce5f3ce01a07ed86da993e487d007b27d1
Mark.Young95 <markyoungday@gmail.com>
2022-03-24 19:24:23 -0700

    component scan?

--- src/main/java/com/revature/AzureFullStackMark/AzureFullStackMarkApplication.java
+++ src/main/java/com/revature/AzureFullStackMark/AzureFullStackMarkApplication.java
@@ -2,8 +2,10 @@ package com.revature.AzureFullStackMark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class AzureFullStackMarkApplication {
 
 	public static void main(String[] args) {
